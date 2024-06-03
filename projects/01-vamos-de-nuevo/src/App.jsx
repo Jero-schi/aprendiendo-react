e91b856d8ef74fbc6efce195a8e4b92a534a0027
Jeronimo <jeronimocomaschi05@gmail.com>
2024-06-03 16:57:35 -0300

    actualizamos para renderizar con array de objs

--- projects/01-vamos-de-nuevo/src/App.jsx
+++ projects/01-vamos-de-nuevo/src/App.jsx
@@ -1,11 +1,48 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
+
+const array = [
+    {
+        "userName": "johndoe123",
+        "name": "John Doe",
+        "isFollowing": true
+    },
+    {
+        "userName": "janedoe456",
+        "name": "Jane Doe",
+        "isFollowing": false
+    },
+    {
+        "userName": "bobski789",
+        "name": "Bob Smith",
+        "isFollowing": true
+    },
+    {
+        "userName": "alicesmith101",
+        "name": "Alice Smith",
+        "isFollowing": false
+    },
+    {
+        "userName": "charlieb102",
+        "name": "Charlie Brown",
+        "isFollowing": true
+    }
+]
+
+
 export function App() {
     return (
         <div className='App'>
-            <TwitterFollowCard initialIsFollowing userName='caracol' name='Jeronimo Comaschi'/>
-            <TwitterFollowCard initialIsFollowing={false} userName='caracoles' name='Jeronimo'/>
-            <TwitterFollowCard initialIsFollowing userName='ardilla' name='Julian'/>
+            {
+                array.map(({userName, name, isFollowing}) => (
+                    <TwitterFollowCard
+                        key={userName}
+                        userName={userName}
+                        name={name}
+                        initialIsFollowing={isFollowing}
+                    />
+                ))
+            }
         </div>
     )
 }
