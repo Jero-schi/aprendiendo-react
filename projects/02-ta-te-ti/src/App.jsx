c9c9a8556d645ec41bf8cd173ebef317f47f4075
Jeronimo <jeronimocomaschi05@gmail.com>
2024-06-10 23:00:12 -0300

    initial projects

---
+++ projects/02-ta-te-ti/src/App.jsx
@@ -0,0 +1,141 @@
+import { useState } from 'react';
+import './App.css'
+
+const TURNS = {
+  X: '❌',
+  O: '⚪'
+}
+
+const WINNERS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+
+const Square = ({children, isSelected, updateBoard, index}) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
+
+const WinnerModal = ({winner, resetGame}) => {
+  if (winner === null) return null
+
+  const winnerText = winner === false ? 'empate' : 'Ganó:'
+
+  return (
+    <section className='winner'>
+      <div className='text'>
+        <h2>{winnerText}</h2>
+
+        <header className='win'>
+          {winner && <Square>{winner}</Square>}
+        </header>
+
+        <footer>
+          <button onClick={resetGame}>Empezar de nuevo</button>
+        </footer>
+      </div>
+    </section>
+  )
+}
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn, setTurn] = useState(TURNS.X)
+  const [winner, setWinner] = useState(null)
+
+  const updateBoard = (index) => {
+    // actualizamos el tablero
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    // cambiar turno
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
+    setTurn(newTurn)
+
+    // verificar si hay un ganador
+    const checkWinnner = (boardToCheck) => {
+      for (const combo of WINNERS) {
+        const [a,b,c] = combo
+
+        if (
+          boardToCheck[a] && 
+          boardToCheck[a] === boardToCheck[b] &&
+          boardToCheck[a] === boardToCheck[c]
+        ) {
+          return boardToCheck[a]
+        }
+      }
+      return null
+    }
+    // verificar si hay empate
+    const checkEndGame = (newBoard) => {
+      return newBoard.every((square) => square !== null)
+    }
+
+    const newWinner = checkWinnner(newBoard)
+    if (newWinner) {
+      setWinner(newWinner)
+      console.log(winner);
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
+    }
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+
+  return (
+    <main className='board'>
+      <h1>Ta Te Ti</h1>
+      <section className='game'>
+        {
+          board.map((square, index) => {
+            return (
+              <Square
+                key={index}
+                index={index}
+                updateBoard={updateBoard}
+              >
+                {square}
+              </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+
+        <WinnerModal resetGame={resetGame} winner={winner}/>
+    </main>
+
+  )
+}
+
+export default App
